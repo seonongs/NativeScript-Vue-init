530b64e2191da20e07c534e796ea5db4f842b68d
seonongs <seonongs@gmail.com>
2020-12-12 22:11:35 +0900

    NativeScript-vue init

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const Vue = require('nativescript-vue/dist/index');
+
+new Vue({
+    template: `
+    <page>
+      <stack-layout>
+        <label text="Hello Vue!" style="background-color:#41b883;color:#ffffff;padding:10;text-align:center"></label>
+      </stack-layout>
+    </page>
+  `,
+}).$start();
